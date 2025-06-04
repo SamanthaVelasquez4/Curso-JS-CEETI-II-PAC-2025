f628c3633b6c78fc896731ac5c343326b2c137c3
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-06-04 14:08:39 -0600

    Clase 2 variables y operadores

---
+++ Semana 1/Clase 2/script.js
@@ -0,0 +1,27 @@
+console.log("Hola mundo!"); //imprimir en pantalla --Salida
+
+//Crear variable
+let cadena; //Declarar una variable
+console.log(cadena);
+cadena = "Palabra"; //inicializar variable = darle un valor
+
+console.log(cadena);
+
+let numero = 5.2753; //declaramos e inicializamos al mismo tiempo
+let primerNumero = 5; //lower camel case
+let segundoNumero = 20;
+console.log(numero);
+
+let booleano = true;
+console.log(booleano);
+
+//operadores aritmeticos
+let suma = primerNumero + segundoNumero;
+let resta = numero - primerNumero;
+let division = numero / segundoNumero;
+let multiplicacion = primerNumero * numero;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
---
+++ Semana 1/Clase 3/script.js
@@ -0,0 +1,61 @@
+//CONDICIONALES
+
+/* IF, ELSE, ELSE IF
+if (condición) {
+    --code to run if condition is true 
+} else if (condition){
+    --code to run if condition is true 
+}else {
+    --run some other code instead 
+}
+*/
+
+/*Ejemplo con IF 
+    Verificar si un número es positivo.
+*/
+
+
+/*Ejemplo con IF, ELSE
+    Verificar si un número es par o impar.
+*/
+
+
+/*Ejemplo con IF, ELSE IF, ELSE
+    Clasificar una temperatura.
+    Igual o menor a 10 -> frio
+    entre 10 y 25 -> agradable
+    mayor de 25 -> calor
+*/
+
+
+/* SWITCH
+switch (expression) {
+  case choice1:
+    // run this code
+    break;
+
+  case choice2:
+    // run this code instead
+    break;
+
+  // include as many cases as you like
+
+  default:
+    // actually, just run this code
+    break;
+}
+*/
+
+/* Ejemplo de switch
+    Evaluación de notas
+    nota es mayor que 8.5 -> Excelente
+    nota esta entre 6 y 8.5 -> Aprobado
+    nota igual a 6 -> Obtuviste el puntaje minimo para pasar
+    nota es menor que 6 -> Reprobado
+*/
+
+
+
+//Normalmente los switches se utilizan para menus
+
+
