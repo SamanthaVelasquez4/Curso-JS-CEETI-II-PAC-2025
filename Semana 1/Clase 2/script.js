a055a46d16ef11a7faf7b77cf9cd39c112c01b87
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-06-04 14:25:17 -0600

    Operadores

--- Semana 1/Clase 2/script.js
+++ Semana 1/Clase 2/script.js
@@ -1,19 +1,19 @@
-console.log("Hola mundo!"); //imprimir en pantalla --Salida
+//console.log("Hola mundo!"); //imprimir en pantalla --Salida
 
 //Crear variable
 let cadena; //Declarar una variable
-console.log(cadena);
+//console.log(cadena);
 cadena = "Palabra"; //inicializar variable = darle un valor
 
-console.log(cadena);
+//console.log(cadena);
 
 let numero = 5.2753; //declaramos e inicializamos al mismo tiempo
 let primerNumero = 5; //lower camel case
 let segundoNumero = 20;
-console.log(numero);
+//console.log(numero);
 
 let booleano = true;
-console.log(booleano);
+//console.log(booleano);
 
 //operadores aritmeticos
 let suma = primerNumero + segundoNumero;
@@ -21,7 +21,28 @@ let resta = numero - primerNumero;
 let division = numero / segundoNumero;
 let multiplicacion = primerNumero * numero;
 
-console.log(suma);
+/*console.log(suma);
 console.log(resta);
 console.log(multiplicacion);
-console.log(division);
+console.log(division);*/
+
+/*operadores de comparacion
+< menor que
+> mayor que
+== igual que
+<= menor o igual que
+>= mayor o igual que
+*/
+
+console.log(100<20);
+console.log(suma == resta);
+console.log(suma>=resta);
+console.log(division<=multiplicacion);
+
+/*operadores logicos
+y (&&)
+o (||)
+*/
+
+console.log(true && false && true); //con que una condicion sea falsa, todo es falso
+console.log(false || false || false); //todas las condiciones tienen que ser falsas para que todo sea falso
